3bd4a28568b81990d1d86ad1cbf965025e43e610
yanxubuchifan <1206849098@qq.com>
2025-11-12 10:02:55 +0800

    date1111

--- app/src/main/java/com/cnpc/myapplication/FamilyAdapter.kt
+++ app/src/main/java/com/cnpc/myapplication/FamilyAdapter.kt
@@ -6,30 +6,31 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+
 class FamilyAdapter(private val familyList: List<PersonInfo.FamilyMember>) :
-    RecyclerView.Adapter<FamilyAdapter.ViewHolder>() {
+    RecyclerView.Adapter<FamilyAdapter.FamilyViewHolder>() {
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val relationship: TextView = itemView.findViewById(R.id.relationship)
-        val name: TextView = itemView.findViewById(R.id.name)
-        val birthday: TextView = itemView.findViewById(R.id.birthday)
-        val political: TextView = itemView.findViewById(R.id.political)
-        val position: TextView = itemView.findViewById(R.id.position)
+    inner class FamilyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val relationText: TextView = itemView.findViewById(R.id.relation_text)
+        val nameText: TextView = itemView.findViewById(R.id.name_text)
+        val birthdayText: TextView = itemView.findViewById(R.id.birthday_text)
+        val politicalText: TextView = itemView.findViewById(R.id.political_text)
+        val positionText: TextView = itemView.findViewById(R.id.position_text)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FamilyViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.family_table_item, parent, false)
-        return ViewHolder(view)
+            .inflate(R.layout.item_family_member, parent, false)
+        return FamilyViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FamilyViewHolder, position: Int) {
         val member = familyList[position]
-        holder.relationship.text = member.oneinfo_family_mamber_relationship
-        holder.name.text = member.oneinfo_family_mamber_name
-        holder.birthday.text = member.oneinfo_family_mamber_birthday
-        holder.political.text = member.oneinfo_family_mamber_political
-        holder.position.text = member.oneinfo_family_mamber_position
+        holder.relationText.text = member.oneinfo_family_mamber_relationship
+        holder.nameText.text = member.oneinfo_family_mamber_name
+        holder.birthdayText.text = member.oneinfo_family_mamber_birthday
+        holder.politicalText.text = member.oneinfo_family_mamber_political
+        holder.positionText.text = member.oneinfo_family_mamber_position
     }
 
     override fun getItemCount() = familyList.size
